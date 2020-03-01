ae1f2af62d09d1052aac956b236c67e8f28232b7
sarahmarie23 <57870970+sarahmarie23@users.noreply.github.com>
2020-03-01 08:44:20 -0800

    Add files via upload
    I've made updates based on the comments on everything except for the one about Stringbuilder (because I haven't used it before so I'll work on that and post another update after.)

--- PizzaIpsum.java
+++ PizzaIpsum.java
@@ -1,37 +1,41 @@
 import java.util.*;
 public class PizzaIpsum {
+    private static Random RANDOM = new Random();
+    
     public static void main(String[] args) {
         System.out.println("How many paragraphs would you like?");
         Scanner input = new Scanner(System.in);
         int paragraphCount = input.nextInt();
-        Random rand = new Random();
-        pageGenerator(paragraphCount);   
+        String pizzaWords[] = new String[] {"pizza", "dough", "pepperoni", "pizza hut", "oven", "mozzarella", "cheese", "sauce", "pineapple", "mushrooms", "olives", "peppers"};
+        generatePage(paragraphCount, pizzaWords);   
     }
         
-    public static void sentenceGenerator () {
-        String pizzaWords[] = new String[] {"pizza", "dough", "pepperoni", "pizza hut", "oven", "mozzarella", "cheese", "sauce", "pineapple", "mushrooms", "olives", "peppers"};
-        Random rand = new Random();
-        int sentenceLength = rand.nextInt(6)+1;
-        
-        String firstWord = pizzaWords[sentenceLength].substring(0, 1).toUpperCase() + pizzaWords[sentenceLength].substring(1);
+    public static void generateSentence (String pizzaWords[]) {
+        int MAX_SENTENCE_LENGTH = RANDOM.nextInt (9-3 +1);
+        String firstWord = capitalizeFirstWord(pizzaWords, MAX_SENTENCE_LENGTH);
         System.out.print(firstWord);
     
-        for(int word = 0; word < sentenceLength; word++){
+        for(int word = 0; word < MAX_SENTENCE_LENGTH; word++){
             System.out.print(" " + pizzaWords[word]);
         }
         System.out.print(". ");
     }
     
-    public static void paragraphGenerator() {
-        Random rand = new Random();
-        for(int sentence = 0; sentence < rand.nextInt(10)+2; sentence++) {
-            sentenceGenerator();
+    public static String capitalizeFirstWord(String pizzaWords[], int MAX_SENTENCE_LENGTH) {
+        String firstWord = pizzaWords[MAX_SENTENCE_LENGTH].substring(0, 1).toUpperCase() + pizzaWords[MAX_SENTENCE_LENGTH].substring(1);
+        return firstWord;
+    }
+    
+    public static void generateParagraph(String pizzaWords[]) {
+        int MAX_PARAGRAPH_LENGTH = RANDOM.nextInt (12-4 +1);
+        for(int sentence = 0; sentence < MAX_PARAGRAPH_LENGTH; sentence++) {
+            generateSentence(pizzaWords);
         }
     }
     
-    public static void pageGenerator (int paragraphCount) {
+    public static void generatePage (int paragraphCount, String pizzaWords[]) {
         for(int paragraph = 0; paragraph < paragraphCount; paragraph++) {
-            paragraphGenerator();
+            generateParagraph(pizzaWords);
             System.out.println();
         }
     }
